4e66fd3aaa6f44a240fd7f8774619ea1d533b5d4
JonB <barbituriko15@gmail.com>
2014-05-06 13:31:36 +0100

    Metodo para crear modelos de JTable(sin acabar)

---
+++ Vecinosenred/src/metodos/CrearTableModel.java
@@ -0,0 +1,17 @@
+package metodos;
+
+import javax.swing.table.DefaultTableModel;
+
+public class CrearTableModel {
+	
+	private DefaultTableModel tableModel;
+	private int columnas,filas;
+	private Object[] datos;
+
+	public CrearTableModel(String tabla){
+		
+		
+		
+	}
+	
+}
